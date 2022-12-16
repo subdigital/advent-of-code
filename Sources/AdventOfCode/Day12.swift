e00c2cc62d1a23017460d604f65f419bbe24d5db
Ben Scheirman <ben@scheirman.com>
2022-12-16 08:09:19 -0600

    Day 12

--- Sources/AdventOfCode/Day12.swift
+++ Sources/AdventOfCode/Day12.swift
@@ -9,39 +9,27 @@ struct Day12: Challenge {
 
         let terrain = Grid.parse(input)
         print(terrain)
-        output.append("Part 1: \(part1(terrain: terrain))")
-        // output.append("Part 2: \(part2(group))")
+        output.append("Part 1: \(part1(terrain: terrain))\n")
+        output.append("Part 2: \(part2(terrain: terrain))")
 
         return output
     }
 
     func part1(terrain: Grid) -> String {
-        let pathFinder = PathFinder(terrain: terrain)
+        let pathFinder = PathFinder(terrain: terrain, neighborFilter: neighborsWithMaxUpill(1))
         let start = terrain.find("S")!
         let end = terrain.find("E")!
-
         let path = pathFinder.shortestPath(from: start, to: end)
-        for segment in zip(path, path.dropFirst()) {
-            let dx = segment.1.x - segment.0.x
-            let dy = segment.1.y - segment.0.y
-            if dx > 0 {
-                print(">")
-            } else if dx < 0 {
-                print("<")
-            } else if dy > 0 {
-                print("v")
-            } else if dy < 0 {
-                print("^")
-            } else {
-                fatalError()
-            }
-        }
+        return "Number of steps: \(path.count)"
+    }
 
-        return ""
+    func part2(terrain: Grid) -> String {
+        let pathFinder = PathFinder(terrain: terrain, neighborFilter: neighborsWithMaxDownhill(-1))
+        let end = terrain.find("E")!
+        let path = pathFinder.shortestPath(from: end, until: { terrain[$0] == "a" })
+        return "Number of steps: \(path.count)"
     }
-    func part2() -> String {
-        ""
-    }
+
     struct Node: Hashable, CustomStringConvertible, CustomDebugStringConvertible {
         let x: Int
         let y: Int
@@ -80,10 +68,6 @@ struct Day12: Challenge {
             }
         }
     }
-    struct Pair<A: Hashable, B: Hashable>: Hashable {
-        let a: A
-        let b: B
-    }
     struct Path: Hashable, Comparable {
         static func < (lhs: Day12.Path, rhs: Day12.Path) -> Bool {
             lhs.cost < rhs.cost
@@ -92,24 +76,24 @@ struct Day12: Challenge {
         let b: Node
         let cost: Int
     }
+
+    struct CostResult {
+        let distancesFromStartNode: [Node: Int]
+        let bestPaths: [Node: Node]
+        let endNode: Node
+    }
+
     struct PathFinder {
         let terrain: Grid
+        let neighborFilter: (Grid, (Int, Int), Int) -> Bool
 
-
-        init(terrain: Grid) {
-            self.terrain = terrain
-        }
-
-        func shortestPath(from: (Int, Int), to: (Int, Int)) -> [Node] {
-            shortestPath(from: from, stop: { $0 == to })
-        }
-
-        func shortestPath(from: (Int, Int), stop: ((Int, Int)) -> Bool) -> [Node] {
-            var dist: [Node: Int] = [:]
+        private func computeCosts(from: (Int, Int), until: ((Int, Int)) -> Bool) -> CostResult {
             let startNode = Node(from)
-            let destNode = Node(to)
 
-            // best distance to get to this node
+            // costs from the start node to every other node
+            var dist: [Node: Int] = [:]
+
+            // find the best path from a node to any other node (with its cost)
             var bestPaths: [Node: (Node, Int)] = [:]
 
             var pq = PriorityQueue<Path>()
@@ -120,9 +104,11 @@ struct Day12: Challenge {
             dist[startNode] = 0
             pq.enqueue(element: .init(a: startNode, b: startNode, cost: 0))
 
+            var dest: Node?
+
             while let nextMinPath = pq.dequeue() {
                 let node = nextMinPath.b
-                print("visiting node \(node), cost is \(nextMinPath.cost)")
+                print("visiting node \(node), cost is \(nextMinPath.cost) [\(terrain[node.pos]!)]")
 
                 if nextMinPath.cost > dist[node, default: .max] {
                     // we've already found a better path, skip it
@@ -130,15 +116,20 @@ struct Day12: Challenge {
                     continue
                 }
                 visited.insert(node)
-                for n in terrain.neighbors(node.pos) {
+
+                if until(node.pos) {
+                    dest = node
+                    break
+                }
+
+                let validNeighbors = terrain.neighbors(node.pos).filter { n in
+                    neighborFilter(terrain, n.pos, n.cost)
+                }
+
+                for n in validNeighbors {
                     let neighborNode = Node(n.pos)
                     guard !visited.contains(neighborNode) else { continue }
                     print("\t neighbor (\(n.pos)) -> \(n.cost) ")
-
-                    if n.cost > 1 {
-                        print("\t unreachable...")
-                        continue
-                    }
                     let costFromStart = n.cost + nextMinPath.cost + 1
                     print("\t\t fromStart \(costFromStart)")
                     if costFromStart < dist[neighborNode, default: .max] {
@@ -154,19 +145,55 @@ struct Day12: Challenge {
                 }
             }
 
-            var pathMap = Grid(data: .init(repeating: ".", count: terrain.width * terrain.height), width: terrain.width, height: terrain.height)
-            pathMap.replace(char: "E", at: destination)
+            guard let dest else {
+                fatalError("Went through the grid but did not find a valid path to the end node!")
+            }
 
+            return .init(
+                distancesFromStartNode: dist,
+                bestPaths: bestPaths.mapValues { $0.0 },
+                endNode: dest
+            )
+        }
+
+        func shortestPath(from: (Int, Int), to: (Int, Int)) -> [Node] {
+            let costs = computeCosts(from: from, until: { $0 == to })
+            let bestPaths = costs.bestPaths
+
+            var pathMap = Grid(data: .init(repeating: ".", count: terrain.width * terrain.height), width: terrain.width, height: terrain.height)
+            pathMap.replace(char: "E", at: to)
+
+            let startNode = Node(from)
             var path: [Node] = []
-            var current = destNode
+            var current = Node(to)
             while current != startNode {
                 path.append(current)
-                let (pathVia, _) = bestPaths[current]!
+                let pathVia = bestPaths[current]!
                 pathMap.replace(char: pathVia.direction(to: current), at: pathVia.pos)
                 current = pathVia
             }
             print(pathMap)
-            print("Number of steps: \(path.count)")
+            return path.reversed()
+        }
+
+        func shortestPath(from: (Int, Int), until: ((Int, Int)) -> Bool) -> [Node] {
+            let costs = computeCosts(from: from, until: until)
+            let bestPaths = costs.bestPaths
+
+            let startNode = Node(from)
+            var path: [Node] = []
+            var current = costs.endNode
+
+            var pathMap = Grid(data: .init(repeating: ".", count: terrain.width * terrain.height), width: terrain.width, height: terrain.height)
+            pathMap.replace(char: "E", at: from)
+
+            while current != startNode {
+                path.append(current)
+                let pathVia = bestPaths[current]!
+                pathMap.replace(char: pathVia.direction(to: current), at: pathVia.pos)
+                current = pathVia
+            }
+            print(pathMap)
             return path.reversed()
         }
     }
@@ -268,10 +295,22 @@ struct Day12: Challenge {
             }
             return output
         }
-
-        // func column(_ c: Int) -> [Int] {
-        //     assert(c >= 0 && c < width)
-        //     return transposed[c]
-        // }
     }
+
+    // MARK: - Neighbor Filters
+
+    /// Returns a neighbor if it's at most `max` higher than the current node
+    func neighborsWithMaxUpill(_ max: Int) -> (Grid, (Int, Int), Int) -> Bool {
+        return { _, _, cost in
+            cost <= max
+        }
+    }
+
+    /// Returns a neighbor if it's not too steep down
+    func neighborsWithMaxDownhill(_ max: Int) -> (Grid, (Int, Int), Int) -> Bool {
+        return { _, _, cost in
+            cost >= max
+        }
+    }
+
 }
